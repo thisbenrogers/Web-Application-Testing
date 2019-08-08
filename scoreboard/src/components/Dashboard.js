a0802d1fd7fd3c4b71f4262a423a7f5ec7553659
BenR <20133902+thisbenrogers@users.noreply.github.com>
2019-08-08 15:24:51 -0500

    :sparkles: Completes Functionality
    Adds strike, ball, hit, and foul buttons with functionality

--- scoreboard/src/components/Dashboard.js
+++ scoreboard/src/components/Dashboard.js
@@ -2,12 +2,49 @@ import React from 'react';
 
 const Dashboard = props => {
 
+  const {
+    reset,
+    strikes,
+    balls,
+    setStrikes,
+    setBalls
+  } = props;
+
+  const clickStrike = e => {
+    e.preventDefault();
+    if (strikes < 2) {
+      setStrikes(strikes + 1)
+    } else {
+      reset();
+    }
+  }
+
+  const clickBall = e => {
+    e.preventDefault();
+    if (balls < 3) {
+      setBalls(balls + 1)
+    } else {
+      reset();
+    }
+  }
+
+  const clickFoul = e => {
+    e.preventDefault();
+    if (strikes === 1) {
+      setStrikes(strikes + 1);
+    } else if (strikes === 0) {
+      setStrikes(strikes + 2)
+    } else {
+      return;
+    }
+  }
+
   return (
     <>
-      <button>Strike</button>
-      <button>Ball</button>
-      <button>Foul</button>
-      <button>Hit</button>
+      <button onClick={e => clickStrike(e)}>Strike</button>
+      <button onClick={e => clickBall(e)}>Ball</button>
+      <button onClick={e => clickFoul(e)}>Foul</button>
+      <button onClick={() => reset()}>Hit</button>
     </>
   )
 }
